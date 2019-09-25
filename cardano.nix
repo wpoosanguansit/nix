bbde43b38c6fa8af57545200d1e0c41140cb5e06
wpoosanguansit <wpoosanguansit@yahoo.com>
2019-09-25 10:36:04 -0700

    Commit update to xserver section.

--- cardano.nix
+++ cardano.nix
@@ -4,20 +4,26 @@
   boot.loader.grub.enable = false;
 
   # if you have a Raspberry Pi 2 or 3, pick this:
-  boot.kernelPackages = pkgs.linuxPackages;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   # A bunch of boot parameters needed for optimal runtime on RPi 3b+
+  boot.kernelParams = ["cma=256M"];
   boot.loader.raspberryPi.enable = true;
   boot.loader.raspberryPi.version = 3;
   boot.loader.raspberryPi.uboot.enable = true;
-
-  # Extra packages
+  boot.loader.raspberryPi.firmwareConfig = ''
+    gpu_mem=256
+  '';
   environment.systemPackages = with pkgs; [
-    vim
+    raspberrypi-tools
   ];
 
   # File systems configuration for using the installer's partition layout
   fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXOS_BOOT";
+      fsType = "vfat";
+    };
     "/" = {
       device = "/dev/disk/by-label/NIXOS_SD";
       fsType = "ext4";
